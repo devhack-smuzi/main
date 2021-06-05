319f1193a769bc24d3c1f89f3d1724bc1f29212b
wecoastca <foxconq@gmail.com>
2021-06-05 14:33:32 +0300

    Define main page configuration

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index",
+  cache: false,
+
+  mode: "development",
+  devtool: "source-map",
+
+  optimization: {
+    minimize: false,
+  },
+
+  output: {
+    publicPath: "http://localhost:3005/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".js", ".json"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "main",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["main"],
+    }),
+  ],
+};
