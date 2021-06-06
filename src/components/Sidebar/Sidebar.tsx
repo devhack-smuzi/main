024ad45f8040dd4c61fff1a753b5761e88519914
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-06-06 03:26:55 +0300

    Update Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,34 @@
 import { Button, Menu, Space } from 'antd';
 import { Avatar, Layout } from 'antd';
+import { MenuInfo } from 'rc-menu/lib/interface';
+
 import React, { FC } from 'react';
 import SettingOutlined from '@ant-design/icons/SettingOutlined';
 import MailOutlined from '@ant-design/icons/MailOutlined';
 
 const { Sider, Content } = Layout;
 
-const Sidebar = () => {
+type Props = {
+  onChange?: (name: string) => void;
+};
+
+const Sidebar = ({ onChange }: Props) => {
+  const handleOnClick = (e: MenuInfo) => {
+    onChange && onChange(e.key);
+  };
+
   return (
     <Sider width={200} style={{ height: '100vh' }}>
       <Menu
         mode="inline"
+        onClick={handleOnClick}
         style={{
           height: '100%',
           borderRight: 0,
           backgroundColor: '#323242',
           color: 'white',
         }}>
-        <Menu.Item key="1" style={{ height: '14vh', alignItems: 'center' }}>
+        <Menu.Item key="home" style={{ height: '14vh', alignItems: 'center' }}>
           <Space direction="horizontal">
             <Avatar
               size={64}
@@ -36,10 +47,10 @@ const Sidebar = () => {
             />
           </Space>
         </Menu.Item>
-        <Menu.Item key="2">Карты</Menu.Item>
-        <Menu.Item key="3">Вклады и счета</Menu.Item>
-        <Menu.Item key="4">Кредиты</Menu.Item>
-        <Menu.Item key="5">Инвестиции</Menu.Item>
+        <Menu.Item key="cards">Карты</Menu.Item>
+        <Menu.Item key="account">Вклады и счета</Menu.Item>
+        <Menu.Item key="credits">Кредиты</Menu.Item>
+        <Menu.Item key="invest">Инвестиции</Menu.Item>
       </Menu>
     </Sider>
   );
